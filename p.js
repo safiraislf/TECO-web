59d48e0e1ea7e518c3cda0c7f80dd3f934a7a857
Safira Sulfa <105907570+safiraislf@users.noreply.github.com>
2023-06-14 14:28:38 +0700

    TECO
    Website TECO (Technology Course)

---
+++ p.js
@@ -0,0 +1,7 @@
+// A function to display a message
+function sayHello() {
+    alert("Sign Up Succesfully!");
+}
+ 
+// Call function on click of the button
+document.getElementById("myBtn").onclick = sayHello;
